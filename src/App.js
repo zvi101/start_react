0cad892eaf67cb5740e4c48152b27857be228cbf
zvi101 <zvikidzll01@gmail.com>
2024-01-15 22:57:47 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>start react</h1>
+      <button className='btn btn-success'>click</button>
+    </div>
+  );
+}
+
+export default App;
